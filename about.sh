787c94bd410cf48e8b2ea2ed6ae7b14ef6019c58
jm051484 <31314849+jm051484@users.noreply.github.com>
2019-06-18 21:25:35 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "Script Auto Install SSH & OpenVPN v2.0" | lolcat 
+echo -e ""
+echo -e "For Debian 8 32 bit & 64 bit"
+echo -e "For VPS with KVM and VMWare virtualization"
+echo -e ""
+echo -e "Original script by :"
+echo -e "* Fornesia"
+echo -e "* Rzengineer"
+echo -e "* Fawzya"
+echo -e ""
+echo -e "Mod by jm051484"
+echo -e ""
+echo -e "jm051484@gmail.com"
+echo -e ""
